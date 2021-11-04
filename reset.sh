e060148ffbecaa0095663f8f5ed3d044f4a1966e
Dayan Eam <eam.dayan@gmail.com>
2021-11-04 09:28:19 +0700

    update reset

--- reset.sh
+++ reset.sh
@@ -4,4 +4,14 @@ rm -rf ./artifacts/* 2> /dev/null
 rm -rf ./blocks-inspected/* 2> /dev/null
 rm -rf ./organizations/* 2> /dev/null
 rm -rf ./ledger/* 2> /dev/null
+rm -rf ./lifecycle/* 2> /dev/null
+rm -rf ./chaincodes/* 2> /dev/null
+rm -rf ./externalbuilder/* 2> /dev/null
+rm -rf ./ledgersData/* 2> /dev/null
+rm -rf ./lifecycle/* 2> /dev/null
+rm -rf ./snapshots/* 2> /dev/null
+rm -rf ./transientstore/* 2> /dev/null
+rm mychannel.block
+
+
 
